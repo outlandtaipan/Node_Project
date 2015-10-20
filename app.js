5c844bf6740ca045bd89361c6bc45f1362f4cee6
vidic.dejan@hotmail.com <FCBarcelona@1>
2015-10-20 11:53:47 +0200

    Removed nonsense code

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var x = 0;
-
 var app = express();
 
 // view engine setup
